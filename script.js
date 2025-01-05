16af09bba4293f089586d57119bb2ad22b1474e1
ajitechcode <143255626+pstar8@users.noreply.github.com>
2025-01-05 21:44:12 +0100

    updated markup, styles and script
    Mobile version now better, script still needs correction.

--- script.js
+++ script.js
@@ -5,12 +5,18 @@ document.addEventListener('DOMContentLoaded', () => {
       const faqContainer = faqGroup.querySelector('.question');
       const icon = faqGroup.querySelector('.faq_icon');
       const response = faqGroup.querySelector('.response');
+      const minusIcon = faqGroup.querySelector('.minus');
+      const plusIcon = faqGroup.querySelector('.plus');
   
-      faqContainer.addEventListener('click', (event) => {
+      faqContainer.addEventListener('click', function(){ 
+        this. classList.toggle('open');
         // Toggle open/closed state
         icon.classList.toggle('open');
         response.classList.toggle('open');
-  
+        minusIcon.style.display = 'block';
+        plusIcon.style.display = 'none';
+        
+       
         // Close other FAQs
         faqGroups.forEach((otherGroup) => {
           if (otherGroup !== faqGroup) {
