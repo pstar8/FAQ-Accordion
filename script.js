067272948708509389c60221ac0944324420d333
ajitechcode <143255626+pstar8@users.noreply.github.com>
2025-02-08 01:16:32 +0100

    updated script and styles
    Accordion now works but I have to work on the icon

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const faqGroups = document.querySelectorAll('.accordion');
-  
+    let currentAccordion = ""
     faqGroups.forEach((faqGroup) => {
-      const faqContainer = faqGroup.querySelector('.question');
+      const faqContainer = faqGroup.querySelectorAll('.question');
       const icon = faqGroup.querySelector('.faq_icon');
       const response = faqGroup.querySelector('.response');
       const minusIcon = faqGroup.querySelector('.minus');
       const plusIcon = faqGroup.querySelector('.plus');
-  
-      faqContainer.addEventListener('click', function(){ 
-        this. classList.toggle('open');
-        // Toggle open/closed state
-        icon.classList.toggle('open');
-        response.classList.toggle('open');
-        minusIcon.style.display = 'block';
-        plusIcon.style.display = 'none';
-        
-       
+
+      faqContainer.forEach((faq) => {
+        faq.addEventListener('click', function () {
+          // Remove 'open' class from all FAQs
+          faqContainer.forEach((item) => {
+              if (item !== faq) {
+                  item.children[1].classList.remove('open')
+              }
+          });
+          // Toggle 'open' class for the clicked FAQ
+          const faqElements = faq.children;
+          faqElements[1].classList.toggle('open');
+      });
         // Close other FAQs
         faqGroups.forEach((otherGroup) => {
           if (otherGroup !== faqGroup) {
